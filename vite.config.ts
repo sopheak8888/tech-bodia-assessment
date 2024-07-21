6ea3a7c7b2a3798ecb62fdcc09270ef15fa918dc
John <119024403+Bower168@users.noreply.github.com>
2024-07-21 23:29:43 +0700

    chore: Update vite.config.ts to remove base URL configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  base: process.env.NODE_ENV === 'production' ? '/tech-bodia-assessment' : '/'
+  }
 })
