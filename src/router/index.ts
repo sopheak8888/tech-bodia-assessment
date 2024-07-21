a8448b87b32462d3f437bca2f7fe694dea2fcd9e
John <119024403+Bower168@users.noreply.github.com>
2024-07-21 23:35:28 +0700

    chore: Update router configuration and vite.config.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  mode: 'hash',
   routes: [
     {
       path: '/',
