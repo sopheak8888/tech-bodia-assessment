bc878d15f264d826f2a2b645909f432304dff750
sopheak8888 <34706016+sopheak8888@users.noreply.github.com>
2024-07-22 03:29:21 +0000

    chore: Update router configuration and add About page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <script setup lang="ts">
-// import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <!-- <header>
+  <header>
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/" active-class="active-link">Home</RouterLink>
+        <RouterLink to="/about" active-class="active-link">About</RouterLink>
       </nav>
     </div>
-  </header> -->
+  </header>
 
   <RouterView />
 </template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+}
+
+nav {
+  background-color: #3498db;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  font-size: larger;
+}
+
+nav a {
+  color: white;
+  text-decoration: none;
+  margin: 0 1rem;
+}
+
+.active-link {
+  font-weight: bold;
+  color: #000000;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/HomeView.vue')
+      component: () => import('@/views/HomeView.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('@/views/AboutView.vue')
     }
   ]
 })
